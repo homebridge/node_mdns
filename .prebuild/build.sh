c81be53cd53b238fbac9aa70932f294070b59c48
oznu <dev@oz.nu>
2021-02-25 18:10:16 +1100

    prebuild workflow test

--- .prebuild/build.sh
+++ .prebuild/build.sh
@@ -3,10 +3,14 @@
 . /etc/os-release
 
 cd /node-mdns
-# rm -rf node_modules
+rm -rf node_modules
 
-echo "Building for $(uname -m)..."
+echo "Installing modules..."
+
+npm install -g node-gyp
 
 npm install --ignore-scripts
+
+echo "Building for $(uname -m)..."
 
 node .prebuild/buildify.js
--- scripts/install.js
+++ scripts/install.js
@@ -4,12 +4,23 @@ const os = require('os');
 const path = require('path');
 const spawn = require('child_process').spawn;
 
+console.log('Compiling mdns bindings, this may take a few minutes, please wait...')
+
 const gypProcess = spawn(os.platform() === 'win32' ? 'node-gyp.cmd' : 'node-gyp', ['rebuild'], {
   cwd: path.join(__dirname, '..'),
-  stdio: 'inherit'
+  stdio: process.env.MDNS_DEBUG ? 'inherit' : 'ignore' 
+});
+
+gypProcess.on('error', (err) => {
+  console.log(err)
 });
 
 gypProcess.on('exit', function (code) {
+  if (code !== 0) {
+    console.log('Failed to compile mdns bindings; native mdns advertiser option will not be available.')
+  } else {
+    console.log('Succesfully compiled mdns bindings.')
+  }
   // always exit without error
   process.exit(0);
 });
